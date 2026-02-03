71fbca7c41fa78d183955b7da970be0c4d996be2
niw0906 <niw0906@naver.com>
2026-02-03 23:08:10 +0900

    chore: FCM 로그 jsonEncode 추가

--- lib/services/notifications/fcm_notification_service.dart
+++ lib/services/notifications/fcm_notification_service.dart
@@ -11,9 +11,10 @@ class FcmNotificationService {
   static Future<void> init(WidgetRef ref) async {
     /// 포그라운드에선 FCM이 시스템 알림을 자동으로 띄우지 않아서 로컬 알림으로 직접 표시
     FirebaseMessaging.onMessage.listen((message) async {
-      LogUtils.d('onMessage data=${message.data}');
       final n = message.notification;
+
       LogUtils.d('onMessage notification title=${n?.title} body=${n?.body}');
+      LogUtils.d('onMessage data=${jsonEncode(message.data)}');
 
       await LocalNotificationService.show(
         title: n?.title,
@@ -23,31 +24,34 @@ class FcmNotificationService {
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      LogUtils.d('onMessageOpenedApp data=${message.data}');
       final n = message.notification;
+
       LogUtils.d(
         'onMessageOpenedApp notification title=${n?.title} body=${n?.body}',
       );
+      LogUtils.d('onMessageOpenedApp data=${jsonEncode(message.data)}');
     });
 
     FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
     final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
     if (initialMessage != null) {
-      LogUtils.d('getInitialMessage data=${initialMessage.data}');
       final n = initialMessage.notification;
+
       LogUtils.d(
         'getInitialMessage notification title=${n?.title} body=${n?.body}',
       );
+      LogUtils.d('getInitialMessage data=${jsonEncode(initialMessage.data)}');
     }
   }
 }
 
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  LogUtils.d('backgroundMessage data=${message.data}');
   final n = message.notification;
+
   LogUtils.d(
     'backgroundMessage notification title=${n?.title} body=${n?.body}',
   );
+  LogUtils.d('backgroundMessage data=${jsonEncode(message.data)}');
 }
