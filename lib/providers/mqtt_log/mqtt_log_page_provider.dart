8e04520bedad1dd18d7a944c68144637e0179175
niw0906 <niw0906@naver.com>
2026-02-03 23:18:40 +0900

    feat: mqtt_log_page_provider prepend 메소드 추가

--- lib/providers/mqtt_log/mqtt_log_page_provider.dart
+++ lib/providers/mqtt_log/mqtt_log_page_provider.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:smart_feeder_remote/api/mqtt_logs_api.dart';
+import 'package:smart_feeder_remote/models/mqtt_log/item/mqtt_log.dart';
 import 'package:smart_feeder_remote/models/mqtt_log/mqtt_log_page.dart';
 import 'package:smart_feeder_remote/utils/log_utils.dart';
 
@@ -16,11 +17,6 @@ class MqttLogPageProvider extends _$MqttLogPageProvider {
 
   void set(MqttLogPage mqttLogPage) {
     state = mqttLogPage;
-  }
-
-  void clear() {
-    state = null;
-    _isLoadingMore = false;
   }
 
   Future<void> fetchNext() async {
@@ -49,5 +45,18 @@ class MqttLogPageProvider extends _$MqttLogPageProvider {
     } finally {
       _isLoadingMore = false;
     }
+  }
+
+  /// 새 로그를 맨 앞에 추가 (FCM 수신 시 사용)
+  void prepend(MqttLog mqttLog) {
+    final current = state;
+    if (current == null) return;
+
+    state = current.copyWith(items: [mqttLog, ...current.items]);
+  }
+
+  void clear() {
+    state = null;
+    _isLoadingMore = false;
   }
 }
